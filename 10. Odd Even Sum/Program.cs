955d355646e41dab7f6ad6e2c5d573f66f577a54
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-12 05:00:32 +0300

    Add files via upload

---
+++ 10. Odd Even Sum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace OddEvenSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumOdd = 0;
+            int sumEven = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (i % 2 == 0)
+                {
+                    sumEven += num;
+                }
+                else
+                {
+                    sumOdd += num;
+                }
+            }
+            if (sumOdd == sumEven)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine("Sum = " + sumOdd);
+            }
+            else
+            {
+                int diff = Math.Abs(sumEven - sumOdd);
+                Console.WriteLine("No");
+                Console.WriteLine("Diff = " + diff);
+            }
+        }
+    }
+}
