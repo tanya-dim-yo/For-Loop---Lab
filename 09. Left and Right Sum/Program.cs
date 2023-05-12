0d211f9242dd695ebd77305167b66f906303dda4
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-12 04:59:18 +0300

    Add files via upload

---
+++ 09. Left and Right Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                leftSum = leftSum + int.Parse(Console.ReadLine());
+            }
+            for (int i = 1; i <= n; i++)
+            {
+                rightSum = rightSum + int.Parse(Console.ReadLine());
+            }
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine("Yes, sum = " + leftSum);
+            }
+            else
+            {
+                int difference = Math.Abs(rightSum - leftSum);
+                Console.WriteLine("No, diff = " + difference);
+            }
+        }
+    }
+}
