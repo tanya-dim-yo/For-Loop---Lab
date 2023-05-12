f69ed22afed359c8bb66b17ca08ee12dbc2da641
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-05-12 04:58:30 +0300

    Add files via upload

---
+++ 08. Number sequence/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NumberSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double max = -1000000000000000;
+            double min = 1000000000000000;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                if (num > max)
+                {
+                    max = num;
+                }
+                if (num < min)
+                {
+                    min = num;
+                }
+            }
+            Console.WriteLine("Max number: " + max);
+            Console.WriteLine("Min number: " + min);
+        }
+    }
+}
